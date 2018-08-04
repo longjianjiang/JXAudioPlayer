42e5f4409e40743b05fbc813a75b9f11317373b5
jianglongjian <2511067577@qq.com>
2018-08-04 15:20:42 +0800

    Initial Commit

---
+++ JXAudioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXAudioPlayer
+//
+//  Created by zl on 2018/8/4.
+//  Copyright © 2018年 longjianjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JXAudioPlayer/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JXAudioPlayer
+//
+//  Created by zl on 2018/8/4.
+//  Copyright © 2018年 longjianjiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
